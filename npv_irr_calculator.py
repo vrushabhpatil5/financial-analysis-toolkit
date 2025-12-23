8b9b1e37b0801c2e5f3da9103929ff105bb36179
Vrushabh Patil <vrushabhpatil578@gmail.com>
2025-12-23 20:29:38 +0000

    Update npv_irr_calculator.py

--- npv_irr_calculator.py
+++ npv_irr_calculator.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 
-# Load cash flow data
-data = pd.read_csv("sample_cashflows.csv")
-cash_flows = data["CashFlow"].values
+def main():
+    data = pd.read_csv("sample_cashflows.csv")
+    cash_flows = data["CashFlow"].values
 
-discount_rate = 0.10
+    discount_rate = 0.10
 
-npv = np.npv(discount_rate, cash_flows)
-irr = np.irr(cash_flows)
+    npv = np.npv(discount_rate, cash_flows)
+    irr = np.irr(cash_flows)
 
-print("Net Present Value (NPV):", round(npv, 2))
-print("Internal Rate of Return (IRR):", round(irr * 100, 2), "%")
+    print("Net Present Value (NPV):", round(npv, 2))
+    print("Internal Rate of Return (IRR):", round(irr * 100, 2), "%")
+
+if __name__ == "__main__":
+    main()
